2bfc70317c3db1d33b794c9ef4675ca46dd538bf
Shiv Shankar <shivshankar0965@gmail.com>
2023-02-24 11:07:15 +0530

    updated products filter api

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const errorMiddleware = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 // Route Imports
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
